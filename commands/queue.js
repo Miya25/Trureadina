a5092007fa12d80641db67f921575e0f74afaa5e
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-01 23:43:58 -0500

    Improved queue command

--- commands/queue.js
+++ commands/queue.js
@@ -7,36 +7,31 @@ module.exports = {
 	async execute(client, interaction, database) {
 		let bots = await database.Bots.listAll();
 
-		let awaitingBots = bots.filter(
+		const awaitingBots = bots.filter(
 			(bot) => bot.state === "AWAITING_REVIEW"
 		);
-		let claimedBots = bots.filter((bot) => bot.state === "CLAIMED");
-
-		if (awaitingBots.length === 0)
-			awaitingBots = [
-				{
-					username: "There are no bots waiting to be reviewed!",
-					bot_id: "0",
-				},
-			];
-		if (claimedBots.length === 0)
-			claimedBots = [
-				{ username: "There are no claimed bots.", bot_id: "0" },
-			];
+		const claimedBots = bots.filter((bot) => bot.state === "CLAIMED");
 
 		const embed = new client.EmbedBuilder()
 			.setTitle(`Queue`)
 			.setColor("Random")
 			.setDescription(
-				`Unclaimed Bots:\n\t- ${awaitingBots
-					.map(
-						(bot) => `**${bot.username}** | [Invite](${bot.invite})`
-					)
-					.join("\n\t- ")}\n\nClaimed Bots:\n\t- ${claimedBots
-					.map(
-						(bot) => `**${bot.username}** | [Invite](${bot.invite})`
-					)
-					.join("\n\t- ")}`
+				`Unclaimed Bots:\n\t- ${
+					awaitingBots
+						.map(
+							(bot) =>
+								`**${bot.username}** | [Invite](${bot.invite})`
+						)
+						.join("\n\t- ") ||
+					"There are no unclaimed bots to show!"
+				}\n\nClaimed Bots:\n\t- ${
+					claimedBots
+						.map(
+							(bot) =>
+								`**${bot.username}** | [Invite](${bot.invite})`
+						)
+						.join("\n\t- ") || "There are no claimed bots to show!"
+				}`
 			);
 
 		interaction.reply({
