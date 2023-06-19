ab064d903d1d3c7335fdaeca3b92ed9539b22cb9
selectdev <66577268+selectdev@users.noreply.github.com>
2023-06-19 14:41:41 -0500

    Added makefile

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/signal"
-    "syscall"
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "github.com/bwmarrin/discordgo"
-    "github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
@@ -16,12 +16,12 @@ import (
 )
 
 func main() {
-    // Load environment variables from .env file.
-    err := godotenv.Load()
-    if err != nil {
-        fmt.Println("Error loading .env file:", err)
-        return
-    }
+	// Load environment variables from .env file.
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
 
 	// Create a PostgreSQL pool connection.
 	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
@@ -32,38 +32,38 @@ func main() {
 	defer pool.Close()
 
 	// Create a Redis connection
-	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"));
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to Redis%v\n", err)
 		return
 	}
 	client := redis.NewClient(opt)
 
-    // Create a new Discord session using the bot token.
-    dg, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
-    if err != nil {
-        fmt.Println("Error creating Discord session:", err)
-        return
-    }
+	// Create a new Discord session using the bot token.
+	dg, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		fmt.Println("Error creating Discord session:", err)
+		return
+	}
 
-    // Add a handler for the "ready" event.
-    dg.AddHandler(onReady)
+	// Add a handler for the "ready" event.
+	dg.AddHandler(onReady)
 
-    // Open a websocket connection to Discord.
-    err = dg.Open()
-    if err != nil {
-        fmt.Println("Error opening connection:", err)
-        return
-    }
+	// Open a websocket connection to Discord.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection:", err)
+		return
+	}
 
-    // Wait for a termination signal to gracefully close the bot.
-    fmt.Println("Bot is now running. Press CTRL-C to exit.")
-    sc := make(chan os.Signal, 1)
-    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-    <-sc
+	// Wait for a termination signal to gracefully close the bot.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 
-    // Clean up and close the Discord session.
-    dg.Close()
+	// Clean up and close the Discord session.
+	dg.Close()
 
 	// Clean up and close the Database connection
 	pool.Close()
@@ -73,5 +73,5 @@ func main() {
 }
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
-    fmt.Printf("Logged in as %s\n", event.User.String())
+	fmt.Printf("Logged in as %s\n", event.User.String())
 }
