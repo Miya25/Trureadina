a531e005c59cface516e8437d6bb0aba91716f36
frostpaw <frostpaw@frostpaws-MacBook-Air.local>
2023-06-21 22:40:14 +0530

    .

--- main.go
+++ main.go
@@ -13,11 +13,20 @@ func main() {
 
 	bot.CreateBot()
 
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	s := <-sc
+	done := make(chan struct{})
 
-	state.Logger.Errorln("Recieved", s, "signal. Closing...")
-	state.Close()
-	state.Logger.Info("Closed open state connections. Exiting...")
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+		s := <-sc
+
+		state.Logger.Errorln("Recieved", s, "signal. Closing...")
+		state.Close()
+		state.Logger.Info("Closed open state connections. Exiting...")
+
+		close(done)
+	}()
+
+	<-done
+	state.Logger.Info("Recieved done channel close. Exiting...")
 }
