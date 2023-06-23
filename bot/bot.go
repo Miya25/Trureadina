26bb7237b62cdd0e5e0bfab617a3157428edb442
selectdev <66577268+selectdev@users.noreply.github.com>
2023-06-23 14:48:38 -0500

    Added events

--- bot/bot.go
+++ bot/bot.go
@@ -2,17 +2,10 @@ package bot
 
 import (
 	"Trureadina/state"
-	"fmt"
-
-	"github.com/bwmarrin/discordgo"
+	"Trureadina/bot/handlers"
 )
 
 func CreateBot() {
 	// Add a handler for the "ready" event.
-	state.Discord.AddHandler(func(s *discordgo.Session, event *discordgo.Ready) {
-		fmt.Printf("Logged in as %s\n", event.User.String())
-	})
-
-	// Set the presence/status of bot profile
-	state.Discord.UpdateWatchStatus(0, "for updates.")
+	state.Discord.AddHandler(handlers.Ready)
 }
---
+++ bot/handlers/ready.go
@@ -0,0 +1,14 @@
+package handlers;
+
+import (
+	"Trureadina/state"
+	"github.com/bwmarrin/discordgo"
+)
+
+func Ready(s *discordgo.Session, event *discordgo.Ready) {
+	// Tell the console that we officially logged into Discord.
+	state.Logger.Info("Logged in as %s\n", event.User.String())
+
+	// Set the presence/status of bot profile
+	state.Discord.UpdateWatchStatus(0, "Select List")
+}
